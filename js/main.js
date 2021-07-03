fb4b3fd546403419bfddfbc51c336b8da3ad6d0a
Leon3854 <frad109@mail.ru>
2021-07-03 21:09:16 +0500

    Finish work all page

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(function(){
     fade: true,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+      breakpoint: 1246,
+      settings: {
+        dots: false
+      }
+      }
+    ]
   });
 
   $('.reviews__slider').slick({
@@ -12,7 +20,37 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
-    autoplay: true
+    autoplay: true,
+    responsive: [
+      {
+      breakpoint: 1240,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 840,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
   })
 
+
+  $('.menu__btn').click(function() {
+    $(".menu__btn").toggleClass('active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
 });
